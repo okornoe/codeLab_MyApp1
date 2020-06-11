2b6e7577f9d70f4499c12ad45e7f6fc016c84520
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-06-11 09:49:19 +0000

    add onClick listener method to the image view and a method for display toast message

--- app/src/main/java/com/t/myapp1/FirstFragment.java
+++ app/src/main/java/com/t/myapp1/FirstFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -30,5 +31,31 @@ public class FirstFragment extends Fragment {
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
         });
+
+        view.findViewById(R.id.donut).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayToast(R.string.donut_order_message);
+            }
+        });
+
+        view.findViewById(R.id.ice_cream).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayToast(R.string.ice_cream_order_message);
+            }
+        });
+
+        view.findViewById(R.id.froyo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayToast(R.string.froyo_order_message);
+            }
+        });
+    }
+
+    public void displayToast(int message) {
+        Toast.makeText(getActivity(), message,
+                Toast.LENGTH_SHORT).show();
     }
 }
