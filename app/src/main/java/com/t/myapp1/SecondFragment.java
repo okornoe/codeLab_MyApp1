1a511cc168b9403e7ea089ff02c8fcc63cadae12
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-06-12 17:56:59 +0000

    add text view and overide onStart method to receive data from firstFragment

--- app/src/main/java/com/t/myapp1/SecondFragment.java
+++ app/src/main/java/com/t/myapp1/SecondFragment.java
@@ -1,9 +1,11 @@
 package com.t.myapp1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -11,18 +13,21 @@ import androidx.navigation.fragment.NavHostFragment;
 
 public class SecondFragment extends Fragment {
 
+    final static String DATA_RECEIVE = "data_receive";
+    TextView showReceivedData;
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_second, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
+        //TextView showReceivedData = (TextView) fi
         view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -30,5 +35,17 @@ public class SecondFragment extends Fragment {
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
             }
         });
+
+        showReceivedData = view.findViewById(R.id.textview_second);
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Bundle args = getArguments();
+        if (args != null) {
+            showReceivedData.setText(args.getString(DATA_RECEIVE));
+        }
     }
 }
